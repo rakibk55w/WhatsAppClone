ff63761dba7091ce6ba5c1d999176a67e9c94073
rakib <rakibk55w@gmail.com>
2025-06-13 23:20:26 +0600

    Added some necessary assets and utility functions

---
+++ lib/common/utils/device_utility.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+//import 'package:connectivity_plus/connectivity_plus.dart';
+
+class AppDeviceUtils {
+
+  static double getScreenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static double getScreenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static Size getScreenSize(BuildContext context) {
+    return MediaQuery.of(context).size;
+  }
+
+  static double getBottomNavigationBarHeight() {
+    return kBottomNavigationBarHeight;
+  }
+
+  static double getAppbarHeight() {
+    return kToolbarHeight;
+  }
+
+  static void hideStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  static void showStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+  }
+
+  // static Future<bool> hasInternetConnection() async {
+  //   try {
+  //     final result = await InternetAddress.lookup("example.com");
+  //     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+  //   } on SocketException catch (_) {
+  //     return false;
+  //   }
+  // }
+
+  // static Future<bool> hasInternetConnection() async {
+  //   try {
+  //     var connectivityResult = await Connectivity().checkConnectivity();
+  //
+  //     if (connectivityResult == ConnectivityResult.none) {
+  //       _showNoInternetDialog(Get.context!, "Please turn on Wi-Fi or mobile data to continue.");
+  //       return false;
+  //     }
+  //
+  //     final result = await InternetAddress.lookup("example.com");
+  //     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+  //
+  //   } on SocketException catch (_) {
+  //     _showNoInternetDialog(Get.context!, "No internet connection available.");
+  //     return false;
+  //   }
+  // }
+  //
+  // static void _showNoInternetDialog(BuildContext context, String message) {
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return AlertDialog(
+  //         title: const Text("Could not connect to internet."),
+  //         content: Text(message),
+  //         actions: <Widget>[
+  //           TextButton(
+  //             child: const Text("OK"),
+  //             onPressed: () {
+  //               Navigator.of(context).pop();
+  //             },
+  //           ),
+  //         ],
+  //       );
+  //     },
+  //   );
+  // }
+}
