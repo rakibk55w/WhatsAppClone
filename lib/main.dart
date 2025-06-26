684960cd395e9408139ceaca208af0c1dbd65322
rakib <rakibk55w@gmail.com>
2025-06-27 01:23:24 +0600

    added supabase dependencies and safe guarded api key for supabase. also initialized supabase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:whats_app_clone/common/utils/colors.dart';
 import 'package:whats_app_clone/responsive/responsive_layout.dart';
 import 'package:whats_app_clone/responsive/screens/mobile_screen_layout.dart';
 import 'package:whats_app_clone/responsive/screens/tablet_screen_layout.dart';
 import 'package:whats_app_clone/router.dart';
 
+import 'common/env/env.dart';
 import 'features/landing/screens/landing_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(url: 'https://zagbwjiburabqeydosma.supabase.co', anonKey: Env.apiKey);
   runApp(const MyApp());
 }
 
@@ -22,7 +27,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: AppColors.backgroundColor
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(), tabletScreenLayout: TabletScreenLayout()),
+      home: const LandingScreen(),
     );
   }
 }
